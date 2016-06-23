0545878bf44e03a5cede141da254a25cb0f4a197
jitender-axovel <jitender.axovel@gmail.com>
2016-06-23 11:03:23 +0530

    added new migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->bigInteger('fu_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('slug');
+            $table->integer('user_role_id')->unsigned();
+            $table->foreign('user_role_id')->references('id')->on('user_roles');
             $table->string('email')->unique();
             $table->string('password');
+            $table->enum('gender', ['M', 'F']);
+            $table->date('dob');
+            $table->string('profile_pic');
+            $table->boolean('is_blocked');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
---
+++ database/migrations/2016_06_22_121707_create_quizzes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->string('title');
+            $table->string('background_image');
+            $table->text('description');
+            $table->text('pageContent');
+            $table->integer('total_images');
+            $table->text('ogImages');
+            $table->boolean('show_own_profile_picture');
+            $table->boolean('show_image_caption');
+            $table->boolean('active')->default(false)->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('quizzes');
+    }
+}
