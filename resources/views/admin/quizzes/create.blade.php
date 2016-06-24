220bc3c7ef56377907cea6b426939a53255997a1
jitender-axovel <jitender.axovel@gmail.com>
2016-06-24 12:48:54 +0530

    changed form design a little bit

--- resources/views/admin/quizzes/create.blade.php
+++ resources/views/admin/quizzes/create.blade.php
@@ -9,17 +9,16 @@
 			<p class="bold">Create New Quiz</p>
 		</div>
 		<div class="panel-body">
-			<form action="{{ url('admin/quiz') }}" method="POST">
+                        <form action="{{ url('admin/quiz') }}" method="POST" class="form-horizontal">
 				{{csrf_field()}}
-                                <div class="form-group">
-					<label class="control-label col-md-12">Make quiz visible to users?</label>
-					<div class="col-md-12">
-                                                <input type="checkbox" class="form-control" name="active" value="true">
-					</div>
+                                <div class="checkbox-inline col-sm-12">
+                                        <label class="col-md-12">
+                                                <span class="col-sm-3">Make quiz visible to users? </span><span class="col-sm-2"><input type="checkbox" name="active" value="true"></span>
+                                        </label>
 				</div>
                                 <div class="form-group">
-					<label class="control-label col-md-12">Category</label>
-					<div class="col-md-12">
+					<label class="control-label col-sm-2">Category</label>
+					<div class="col-sm-10">
                                             <select name="category_id" class="form-control" >
                                                         @foreach($categories as $category)
                                                                 <option value="{{$category->id}}">{{$category->title}}</option>
@@ -28,8 +27,8 @@
 					</div>
 				</div>
 				<div class="form-group">
-					<label class="control-label col-md-12">Quiz Name</label>
-					<div class="col-md-12">
+					<label class="control-label col-sm-2">Quiz Name</label>
+					<div class="col-sm-10">
 						<input type="text" class="form-control" name="title" value="{{ old('first_name') }}">
 						@if($errors->has('title'))
 							<span class="help-block">
@@ -39,8 +38,8 @@
 					</div>
 				</div>
 				<div class="form-group">
-					<label class="control-label col-md-12">Description</label>
-					<div class="col-md-12">
+					<label class="control-label col-sm-2">Description</label>
+					<div class="col-sm-10">
 						<textarea class="form-control" name="description">{{ old('description') }}</textarea>
 					</div>
 				</div>
