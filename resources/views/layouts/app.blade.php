da334a0f2fcd35db08ad9c849369f4634be79ede
jitender-axovel <jitender.axovel@gmail.com>
2016-06-29 03:48:19 +0530

    changed site name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,14 +54,14 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
+                    FaceQuiz
                 </a>
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
+                    <li><a href="{{ url('/') }}">Home</a></li>
                     <li><a href="{{ url('quizzes/all') }}">{{-- __('ALL') --}}</a></li>
                 </ul>
 
