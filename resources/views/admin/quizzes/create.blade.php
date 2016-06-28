acfb20d99d1ac7c3624366e0a0730e8e997d2645
jitender.axovel@gmail.com <jitender.axovel@gmail.com>
2016-06-28 21:08:49 +0530

    working on quiz creation form

--- resources/views/admin/quizzes/create.blade.php
+++ resources/views/admin/quizzes/create.blade.php
@@ -19,7 +19,7 @@
 			<div class="form-group">
 				<label class="control-label col-sm-2">Quiz Name</label>
 				<div class="col-sm-10">
-					<input type="text" class="form-control" name="title" value="{{ old('title') }}" placeholder="Enter Quiz Title">
+					<input required type="text" class="form-control" name="title" value="{{ old('title') }}" placeholder="Enter Quiz Title">
 					@if($errors->has('title'))
 						<span class="help-block">
 							<strong>{{ $errors->first('title') }}</strong>
@@ -36,7 +36,7 @@
 			<div class="form-group">
 				<label class="control-label col-sm-2">Category</label>
 				<div class="col-sm-10">
-					<select name="category_id" id="category-selector" class="form-control" >
+					<select required name="category_id" id="category-selector" class="form-control" >
 						<option selected>Select Category</option>
 						@foreach($categories as $category)
 							<option value="{{$category->id}}">{{$category->title}}</option>
@@ -47,7 +47,7 @@
 			<div class="form-group">
 				<label class="control-label col-sm-2">Sub-Category</label>
 				<div class="col-sm-10">
-					<select name="sub_category_id" class="form-control" id="sub-category-selector">
+					<select required name="sub_category_id" class="form-control" id="sub-category-selector">
 						<option selected>Select Sub-Category</option>
 						@foreach($subCategories as $subCategory)
 							<option value="{{$subCategory->id}}">{{$subCategory->title}}</option>
@@ -58,13 +58,25 @@
 			<div class="form-group">
 				<label class="control-label col-sm-2">Choose Template</label>
 				<div class="col-sm-10">
-					<select name="quiz_template_id" class="form-control">
+					<select required name="quiz_template_id" class="form-control" id="template-selector">
 						<option selected>Select Template</option>
 						@foreach($templates as $template)
 							<option value="{{$template->id}}">{{$template->name}}</option>
 						@endforeach
 					</select>
 				</div>
+			</div>
+			<div id="template-details">
+				<dl class="dl-horizontal list-inline">
+					<dt>Total images</dt>
+					<dd class="template-images"></dd>
+					<dt>Total Textareas</dt>
+					<dd class="template-textares"></dd>
+					<dt>Title block</dt>
+					<dd class="template-title"></dd>
+					<dt>Image Caption</dt>
+					<dd class="template-caption"></dd>
+				</dl>
 			</div>
 			<div class="form-group">
 				<div class="col-md-12">
@@ -79,6 +91,7 @@
 	<script type="text/javascript">
 		$(document).ready(function() {
 			$('#sub-category-selector').attr('disabled', 'disabled');
+			$('#template-details').hide();
 			$('#category-selector').change(function() {
             	$.ajax({
             		url: "{{url('get-subcategories?ci=')}}" + $(this).val(),
@@ -91,6 +104,21 @@
 					        $("#sub-category-selector").append('<option value="'+ this.id +'">'+ this.title +'</option>')
 					    });
 					    $('#sub-category-selector').removeAttr('disabled');
+            		}
+            	});
+            });
+
+            $('#template-selector').change(function() {
+            	$.ajax({
+            		url: "{{url('get-template-details?ti=')}}" + $(this).val(),
+            		type: "GET",
+            		success: function(data) {
+            			data = JSON.parse(data);
+            			$('.template-images').html(data.total_images);
+            			$('.template-textares').html(data.total_textareas);
+            			$('.template-title').html(data.has_title);
+            			$('.template-caption').html(data.has_image_caption);
+            			$('#template-details').show();
             		}
             	});
             });
