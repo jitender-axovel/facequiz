b7aae5c68b463d3610c918c2e72173d86c532157
jitender.axovel@gmail.com <jitender.axovel@gmail.com>
2016-06-28 11:30:33 +0530

    added new model to handle quiz table

--- app/Quiz.php
+++ app/Quiz.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Quiz extends Model
 {
-    //
+    protected $fillable = ['category_id', 'sub_category_id', 'quiz_template_id', 'title', 'slug', 'background_image', 'description', 'show_own_profile_image', 'show_user_name', 'show_friend_pictures', 'show_friend_name', 'is_active'];
+
+    public static $updatable = ['category_id' => "", 'sub_category_id' => "", 'quiz_template_id' => "", 'title' => "", 'slug' => "", 'background_image' => "", 'description' => "", 'show_own_profile_image' => "", 'show_user_name' => "", 'show_friend_pictures' => "", 'show_friend_name' => "", 'is_active' => ""];
+
+    public function category()
+    {
+    	return $this->belongsTo('App\Category');
+    }
+
+    public function subCategory()
+    {
+    	return $this->belongsTo('App\SubCategory');
+    }
+
+    public function quizTemplate()
+    {
+    	return $this->belongsTo('App\QuizTemplate');
+    }
 }
