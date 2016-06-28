fa75181a173dd3aa0ca64f2929e36be193d5b328
jitender.axovel@gmail.com <jitender.axovel@gmail.com>
2016-06-28 21:10:17 +0530

    added new migrations to create languages tables

---
+++ database/migrations/2016_06_28_111815_create_languages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLanguagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('languages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('code')->unique();
+            $table->string('fb_code')->unique();
+            $table->string('direction');
+            $table->longText('strings');
+            $table->boolean('isActive')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('languages');
+    }
+}
