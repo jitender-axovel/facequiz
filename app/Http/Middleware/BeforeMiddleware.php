ebc6a1174024227b568de2ff3a139d1a96bf82a5
jitender.axovel@gmail.com <jitender.axovel@gmail.com>
2016-06-28 21:09:32 +0530

    added before and after filter

---
+++ app/Http/Middleware/BeforeMiddleware.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+use App;
+
+class BeforeMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //perform action
+        $locale = NULL;
+        $browser_lang = substr($request->server('HTTP_ACCEPT_LANGUAGE'), 0, 2);
+        $cookie_lang = $request->cookie('language');
+        
+        if($cookie_lang && in_array($cookie_lang, config('app.languages'))) {
+            $request->session()->put('locale', $cookie_lang);
+        }
+        elseif(in_array($browser_lang, config('app.languages'))) {
+            $request->session()->put('locale', $browser_lang);
+        }
+        
+        if($request->session()->has('locale')) {
+            $locale = $request->session()->get('locale');
+        }
+        if(!$locale) {
+            $locale = 'en';
+        }
+        // Set the local in Session if it's supported
+        if ( array_key_exists($locale, config('app.locales'))) {
+            $request->session()->put('locale', $locale);
+            // Reload the site without the ending /language part
+        }
+        if(!$request->session()->has('locale'))
+        {
+            $request->session()->put('locale', $request->getPreferredLanguage( config('app.languages')));
+        }
+
+        App::setLocale($request->session()->get('locale'));
+        return $next($request);
+    }
+}
