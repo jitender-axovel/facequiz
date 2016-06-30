ea28bc5a37667753aeb3449c5c057f1175dc83c6
jitender.axovel@gmail.com <jitender.axovel@gmail.com>
2016-06-30 17:59:22 +0530

    ignoring block routes for csrf token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        'admin/users/delete/*'
+        'admin/users/delete/*',
+        'admin/users/block/*'
     ];
 }
