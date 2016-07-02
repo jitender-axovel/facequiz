83ea90d924c932959b26c97b5fded7c429633538
jitender.axovel@gmail.com <jitender.axovel@gmail.com>
2016-07-02 21:00:13 +0530

    model for quiz templates

--- app/QuizTemplate.php
+++ app/QuizTemplate.php
@@ -9,4 +9,15 @@ class QuizTemplate extends Model
 {
     use SoftDeletes;
 	protected $dates = ['deleted_at'];
+
+	protected $fillable = [
+        'name', 'category_id', 'og_image', 'html_data', 'total_images', 'total_textareas', 'has_title', 'has_image_caption'
+    ];
+
+    public static $updatable = ['name' => "", 'category_id' => "", 'og_image' => "", 'html_data' => "", 'total_images' => "", 'total_textareas' => "", 'has_title' => "", 'has_image_caption' => ""];
+
+    public function category()
+    {
+    	return $this->belongsTo('App\Category');
+    }
 }
