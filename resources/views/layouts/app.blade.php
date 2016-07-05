0c0e18a42ce5d31b33585f8d0d26a04e662a37c1
jitender-axovel <jitender.axovel@gmail.com>
2016-07-05 19:26:17 +0530

    rewrite fb login url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,7 +69,7 @@
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="redirect">FB Login</a></li>
+                        <li><a href="{{ url('redirect') }}">FB Login</a></li>
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
