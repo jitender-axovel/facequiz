824e0f6c8b203abbde33270c471a77e3a8a2b8f7
jitender-axovel <jitender.axovel@gmail.com>
2016-07-05 19:25:05 +0530

    added quiz list on the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
+    <div class="container">
+        <div class="row">
+            @include('notification')
+        </div>
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                @foreach($quizzes as $quiz)
+                    <div class="col-md-4 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="{{ url('quiz/'.$quiz->category->slug.'/'.$quiz->slug) }}"><img src="{{ asset('images/templates/'.$quiz->template->og_image) }}"></a>
+                            <div class="caption">
+                                <div class="heading"><a href="{{ url('quiz/'.$quiz->category->slug.'/'.$quiz->slug) }}">{{$quiz->title}}</a></div>
+                                @if($quiz->description != "")
+                                    <p>{{$quiz->description}}</p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
-</div>
 @endsection
