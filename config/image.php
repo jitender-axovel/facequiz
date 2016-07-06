47172fe35c376b4c66e245df24302ca9abb18125
jitender-axovel <jitender.axovel@gmail.com>
2016-07-06 18:47:23 +0530

    added config file for image paths

---
+++ config/image.php
@@ -0,0 +1,49 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+return array(
+
+    /*
+     |.....................................................................
+     |  Quiz paths
+     |.....................................................................
+     |
+     */
+
+    'quiz_template_path' => str_replace('\\','/',public_path()).'/images/quizzes/templates/',
+    'quiz_facts_path' => str_replace('\\','/',public_path()).'/images/quizzes/facts/',
+    
+    /*
+     |.....................................................................
+     |  Quiz urls
+     |.....................................................................
+     |
+     */
+    
+    'quiz_template_url' => '/images/quizzes/templates/',
+    'quiz_facts_path' => str_replace('\\','/',public_path()).'/images/quizzes/facts/',
+
+    /*
+     |.....................................................................
+     |  User Quizzes
+     |.....................................................................
+     |
+     */
+
+    'user_quiz_result_path' => str_replace('\\','/',public_path()).'/images/quizzes/results/',
+    
+    /*
+     |.....................................................................
+     |  User Quizzes urls
+     |.....................................................................
+     |
+     */
+
+    'user_quiz_result_url' => '/images/quizzes/results/',
+
+);
