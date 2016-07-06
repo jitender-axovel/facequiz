25fd7ab9f0f5ae9aa9393d6522d451afe6e6e737
jitender-axovel <jitender.axovel@gmail.com>
2016-07-06 16:37:16 +0530

    prepared quiz playing functionality

--- app/Http/Controllers/QuizzesController.php
+++ app/Http/Controllers/QuizzesController.php
@@ -11,7 +11,7 @@ use App\Category;
 
 class QuizzesController extends Controller
 {
-    public function index($categorySlug, $quizSlug)
+    public function index($quizSlug)
     {
         $quiz = Quiz::where('slug', $quizSlug)->where('is_active', 1)->first();
         
@@ -29,10 +29,34 @@ class QuizzesController extends Controller
     public function start($slug)
     {
         $quiz = Quiz::where('slug', $slug)->where('is_active', 1)->first();
+        
         if(!$quiz) {
             return redirect('/')->with('error', 'Sorry, the quiz you are looking for is unemployed');
         }
         
+        $page = $quiz->title;
+        
+        $template = $quiz->template->html_data;
+        
+        $quizHelper = new \App\QuizHelper();
+        
+        //set title of quiz
+        $template = $quizHelper->setTitle($template, $quiz);
+        
+        //set user profile image
+        $template = $quizHelper->setUserProfileImage($template, $quiz);
+        
+        //set user name
+        $template = $quizHelper->setUserName($template, $quiz);
+        
+        //set friends data
+        $template = $quizHelper->setFriendData($template, $quiz);
+        
+        //set facts
+        $template = $quizHelper->setFacts($template, $quiz);
+        
         $quizzes = Quiz::where('slug', '!=', $slug)->where('is_active', 1)->get();
+        
+        return view('quiz.result', compact('page', 'quiz', 'template', 'quizzes'));
     }
 }
