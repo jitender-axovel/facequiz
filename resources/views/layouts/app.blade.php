e842e5b2b54efca60018058e7a9618aa636b6af8
jitender.axovel@gmail.com <jitender.axovel@gmail.com>
2016-07-26 15:42:22 +0530

    changed footer design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,9 +88,11 @@
 
     @yield('content')
 
-	<footer>
-		<a href="{{url('privacy-policy')}}">Privacy Policy</a>
-</footer>
+    <nav class="navbar navbar-default navbar-fixed-bottom">
+        <div class="container">
+            <a href="{{url('privacy-policy')}}">Privacy Policy</a>
+        </div>
+    </nav>
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
