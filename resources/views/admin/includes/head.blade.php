7117f89a1cb216520cd2ba6e35be1e92b4317916
jitender.axovel@gmail.com <jitender.axovel@gmail.com>
2016-08-05 19:16:25 +0530

    added favicon

--- resources/views/admin/includes/head.blade.php
+++ resources/views/admin/includes/head.blade.php
@@ -5,6 +5,8 @@
 
     <title>@yield('title')</title>
 
+    <link rel="shortcut icon" type="image/png" href="{{ asset('images/favicon.png') }}"/>
+
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,8 @@
 
     <title>@yield('title')</title>
 
+    <link rel="shortcut icon" type="image/png" href="{{ asset('images/favicon.png') }}"/>
+
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
