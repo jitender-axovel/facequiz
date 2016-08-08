0c57f594d3cf0a45e51b652843aa07a77f5cd975
jitender.axovel@gmail.com <jitender.axovel@gmail.com>
2016-08-08 20:55:31 +0530

    removed quiz layout links from sidebar

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -8,17 +8,6 @@
             <a href="{{url('admin/users')}}"><i class="fa fa-user"></i> User Management</a>
         </li>
         <li>
-            <a href="javascript:;" data-toggle="collapse" data-target="#layout" class="collapsed" aria-expanded="false"><i class="fa fa-question"></i> Quiz Layouts <i class="fa fa-fw fa-caret-down"></i></a>
-            <ul id="layout" class="collapse" aria-expanded="false">
-                <li>
-                    <a href="{{url('admin/layout/create')}}">Create New Layout</a>
-                </li>
-                <li>
-                    <a href="{{url('admin/layout')}}">List Layouts</a>
-                </li>
-            </ul>
-        </li>
-        <li>
             <a href="javascript:;" data-toggle="collapse" data-target="#quiz" class="collapsed" aria-expanded="false"><i class="fa fa-question-circle"></i> Quizzes <i class="fa fa-fw fa-caret-down"></i></a>
             <ul id="quiz" class="collapse" aria-expanded="false">
                 <li>
