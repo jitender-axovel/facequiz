4467c4272ff4e322a8b268db84f741b80ccb189b
jitender.axovel@gmail.com <jitender.axovel@gmail.com>
2016-08-13 15:25:57 +0530

    added new view for quiz summary

---
+++ resources/views/quiz/summary.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container main-content">
+        <div class="row advertise-block">
+            @include('includes.above-quizzes-widgets')
+        </div>
+        <div class="row">
+            @include('notification')
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <div class="panel-title heading text-center">{{ $quiz->title }}</div>
+                        </div>
+                        <div class="panel-body">
+                            <div class="fb-like-button">
+                                <div class="loading-image row">
+                                    <span class="lead heading">Kindly Like us on Facebook <i class="fa fa-arrow-right"></i></span>
+                                </div>
+                                @if(isset($fb_like_button))
+                                    {!!$fb_like_button!!}
+                                @else
+                                    <div class="fb-like" data-href="https://www.facebook.com/robodoo.en" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="false"></div>
+                                @endif
+                                <div class="text-center row">
+                                    <a class="btn btn-success btn-lg" href="{{ url('quiz/'.$quiz->slug.'/start') }}"><i class="fa fa-refresh"></i> Try Again</a>
+                                    <a class="btn btn-warning btn-lg" href="{{ url('quiz/'.$quizzes->first()->slug.'/show') }}"><i class="fa fa-angle-double-right"></i>Try Next</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        @if($quizzes->count())
+                            @foreach($quizzes as $quizItem)
+                                <div class="col-md-6 col-sm-6">
+                                    <div class="thumbnail">
+                                        <a href="{{ url('quiz/'.$quizItem->slug.'/show') }}"><img src="{{ asset(config('image.quiz_template_url').$quizItem->template->og_image) }}"></a>
+                                        <div class="caption">
+                                            <div class="heading"><a href="{{ url('quiz/'.$quizItem->slug.'/show') }}">{{$quizItem->title}}</a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @else
+                            <div class="heading">
+                                <span>There are no more quizzes.</span>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+                <div class="row">
+                    @include('includes.after-quiz-widgets')
+                </div>
+            </div>
+            <div class="col-md-4">
+                @include('includes.left-sidebar')
+            </div>
+        </div>
+        <div class="row">
+            @include('includes.below-quizzes-widgets')
+        </div>
+    </div>
+@endsection
