4bcc077e9dd0ab6d49f3336a94f4e4eda91ab76b
jitender-axovel <jitender.axovel@gmail.com>
2016-08-22 19:58:56 +0530

    changed images for widgets

--- database/seeds/WidgetsTableSeeder.php
+++ database/seeds/WidgetsTableSeeder.php
@@ -15,9 +15,9 @@ class WidgetsTableSeeder extends Seeder
         
         DB::table('widgets')->insert([
         	['name' => 'SideBar', 'slug' => 'sidebar', 'preview_image' => 'sidebar.png', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-        	['name' => 'Below Quizzes', 'slug' => 'belowquizzes', 'preview_image' => 'sidebar.png', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-        	['name' => 'Above Quizzes', 'slug' => 'abovequizzes', 'preview_image' => 'sidebar.png', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-        	['name' => 'Below Quiz View', 'slug' => 'belowquizview', 'preview_image' => 'sidebar.png', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        	['name' => 'Below Quizzes', 'slug' => 'belowquizzes', 'preview_image' => 'below-quizzes.png', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        	['name' => 'Above Quizzes', 'slug' => 'abovequizzes', 'preview_image' => 'above-quizzes.png', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        	['name' => 'Below Quiz View', 'slug' => 'belowquizview', 'preview_image' => 'below-quiz-view.png', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
     	]);
     }
 }
