e178bf63a93d7f3f97c84458302e78f52207b836
jitender-axovel <jitender.axovel@gmail.com>
2016-09-22 12:58:56 +0530

    added a little responsive designs

--- resources/views/sections/quiz-item.blade.php
+++ resources/views/sections/quiz-item.blade.php
@@ -1,4 +1,4 @@
-<div class="thumbnail content-img">
+<div class="thumbnail content-img col-md-12 col-xs-12 col-sm-6">
     <div class="quiz-image-wrapper">
         <a href="{{ url('quiz/'.$quizItem->slug.'/show') }}"><img src="{{ asset(config('image.quiz_template_url').$quizItem->template->og_image) }}"></a>
     </div>
