5f0ade798a85efbe89f814b60d4db2250e32b38c
jitender-axovel <jitender.axovel@gmail.com>
2016-09-22 15:58:10 +0530

    changed quiz structure on landing page

--- resources/views/quiz/landing.blade.php
+++ resources/views/quiz/landing.blade.php
@@ -14,20 +14,35 @@
         <div class="row">
             <div class="col-md-9">
                 <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title heading text-center">{{ $quiz->title }}</h3>
-                    </div>
-                    <div class="panel-body">
+                    <div class="panel-body text-center">
                         <div class="img-wrap col-md-10">
                             <img src="{{ asset(config('image.quiz_template_url').$quiz->template->og_image) }}">
                         </div>
+                        @if(!$has_liked_page)
+                            <div class="fb-like-button">
+                                @if(isset($fb_like_button))
+                                    {!!$fb_like_button!!}
+                                @else
+                                    <div class="fb-like" data-href="https://www.facebook.com/robodoo.en" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="false"></div>
+                                @endif
+                            </div>
+                        @endif
+                        <h3 class="panel-title heading text-center">{{ $quiz->title }}</h3>
+                        <p class="quiz-description">{{ $quiz->description }}</p>
                         <div class="caption img-caption">
-                            <a class="btn btn-primary btn-block" href="{{ url('quiz/'.$quiz->slug.'/start/'.md5(time())) }}">{{ $languageStrings['Start'] or 'Start' }}</a>
+                            <a class="btn btn-primary start-with-fb" href="{{ url('quiz/'.$quiz->slug.'/start/'.md5(time())) }}"><i class="fa fa-facebook-official"></i> {{ $languageStrings['Continue with Facebook'] or 'Continue with Facebook' }}</a>
                         </div>
-                        <span>{{ $languageStrings['You will be required to login with Facebook.'] or 'You will be required to login with Facebook.' }}</span>
-                    </div>
-                    <div class="panel-footer">
-                        {{$quiz->description}}
+                        <div class="quiz-options">
+                            <div class="col-md-4">
+                                <a id="shareBtn" class="share-facebook" title="{{ $languageStrings['Share on Facebook'] or 'Share on Facebook' }}"><i class="fa fa-facebook-official"></i> {{ $languageStrings['Share'] or 'Share' }}</a>
+                            </div>
+                            <div class="col-md-4 send-button">
+                                <a id="sendBtn" class="share-facebook" title="{{ $languageStrings['Send Private Message'] or 'Send Private Message' }}"><img src="{{ asset('images/facebook-messanger.png') }}" class="responsive"> {{ $languageStrings['Send'] or 'Send' }}</a>
+                            </div>
+                            <div class="col-md-4">
+                                <a id="copyBtn" class="share-facebook" title="{{ $languageStrings['Copy Link'] or 'Copy Link' }}"><i class="fa fa-link"></i> {{ $languageStrings['Copy Link'] or 'Copy Link' }}</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-12">
@@ -71,4 +86,28 @@
             @include('includes.below-quizzes-widgets')
         </div>
     </div>
+@endsection
+@section('scripts')
+    <script>
+        document.getElementById('shareBtn').onclick = function() {
+            FB.ui({
+                method: 'share',
+                display: 'popup',
+                href: '{{ url("quiz/".$quiz->slug."/show") }}',
+            }, function(response){});
+        }
+
+        document.getElementById('sendBtn').onclick = function() {
+            FB.ui({
+              method: 'send',
+              display: 'popup',
+              // href: '{{ url("quiz/".$quiz->slug."/landing/".Auth::id())."/".md5(time()) }}',
+              link: '{{ url("quiz/".$quiz->slug."/show") }}',
+            });
+        }
+
+        document.getElementById('copyBtn').onclick = function() {
+            prompt('Copy this link', '{{ url("quiz/".$quiz->slug."/landing/".Auth::id())."/".md5(time()) }}');
+        }
+    </script>
 @endsection
