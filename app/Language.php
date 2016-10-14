daabca8be562fec9338b86a16d97ea02fc78043c
jitender-axovel <jitender.axovel@gmail.com>
2016-10-14 23:16:37 +0530

    added order to fillable and updatable array

--- app/Language.php
+++ app/Language.php
@@ -10,7 +10,7 @@ class Language extends Model
 	use SoftDeletes;
 	protected $dates = ['deleted_at'];
 	
-    protected $fillable = ['name', 'code', 'fb_code', 'fb_like', 'fb_page_code', 'fb_widget', 'direction', 'strings'];
+    protected $fillable = ['name', 'code', 'fb_code', 'fb_like', 'fb_page_code', 'fb_widget', 'direction', 'strings', 'order'];
 
-    public static $updatable = ['name' => "", 'code' => "", 'fb_code' => "", 'fb_page_code' => "", 'fb_like' => "", 'fb_widget' => "", 'direction' => "", 'strings' => ""];
+    public static $updatable = ['name' => "", 'code' => "", 'fb_code' => "", 'fb_page_code' => "", 'fb_like' => "", 'fb_widget' => "", 'direction' => "", 'strings' => "", 'order' => ""];
 }
