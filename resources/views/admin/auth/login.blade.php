c6b627069c6ee88ab2d780e2de234f75503fc193
jitender-axovel <jitender.axovel@gmail.com>
2016-10-15 13:51:03 +0530

    fixed design of admin login page

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -4,7 +4,9 @@
     <div class="bg-gray-light" id="frm-login">
         <div class="col-md-4 col-md-offset-4">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading">
+                    <div class="panel-title">Login</div>
+                </div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('admin/login') }}">
                         {!! csrf_field() !!}
@@ -30,19 +32,9 @@
                         </div>
                         <div class="form-group">
                             <div class="col-md-12">
-                                <div class="checkbox">
-                                    <label class="small-text">
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-12">
                                 <button type="submit" class="btn btn-primary btn-lg btn-block" id="btn-login">
                                     <i class="fa fa-btn fa-sign-in"></i>&nbsp;Login
                                 </button>
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
                             </div>
                         </div>
                     </form>
